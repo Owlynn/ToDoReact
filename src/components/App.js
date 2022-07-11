3be2c7e08cc8807f498f07a75458bfb54288fae9
Owlynn <contact@jessalynn.fr>
2022-07-11 12:27:47 +0200

    Création des fichiers + archi + design de base

---
+++ src/components/App.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css'
+
+class App extends React.Component{
+  render(){
+    return ( 
+      <div className='flex-container'>
+          <h1>To-Do-List</h1>
+
+          
+            <div className='list-container'>
+
+                <div className = "item-input">
+                  <input type="text" id="text-input" name="text-input" placeholder='Add a new task'/>
+                  <input type="submit" value="Add Task"></input>
+                </div>
+
+                <fieldset>
+                  
+                  <div className = "list-items">
+                    <input type="checkbox" id="item" name="item" />
+                    <label>Item</label>
+                    <button className='delete-item'> X </button>
+                  </div>
+                  
+                  <div className = "list-items">
+                    <input type="checkbox" id="item" name="item" />
+                    <label>Item</label>
+                    <button className='delete-item'> X </button>
+                  </div>
+                  
+                  <div className = "list-items">
+                    <input type="checkbox" id="item" name="item" />
+                    <label>Item</label>
+                    <button className='delete-item'> X </button>
+                  </div>
+                  
+                  <div className = "list-items">
+                    <input type="checkbox" id="item" name="item" />
+                    <label>Item</label>
+                    <button className='delete-item'> X </button>
+                  </div>
+                  
+                  <div className = "list-items">
+                    <input type="checkbox" id="item" name="item" />
+                    <label>Item</label>
+                    <button className='delete-item'> X </button>
+                  </div>
+                  
+                  <div className = "list-items">
+                    <input type="checkbox" id="item" name="item" />
+                    <label>Item</label>
+                    <button className='delete-item'> X </button>
+                  </div>
+                  
+                  <div className = "list-items">
+                    <input type="checkbox" id="item" name="item" />
+                    <label>Item</label>
+                    <button className='delete-item'> X </button>
+                  </div>
+                  
+                  <div className = "list-items">
+                    <input type="checkbox" id="item" name="item" />
+                    <label>Item</label>
+                    <button className='delete-item'> X </button>
+                  </div>
+                  
+                  <div className = "list-items">
+                    <input type="checkbox" id="item" name="item" />
+                    <label>Item</label>
+                    <button className='delete-item'> X </button>
+                  </div>
+                  
+                  <div className = "list-items">
+                    <input type="checkbox" id="item" name="item" />
+                    <label>Item</label>
+                    <button className='delete-item'> X </button>
+                  </div>
+                 
+              </fieldset>
+            </div>
+    </div>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+export default App;
