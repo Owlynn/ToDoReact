ebea237a8f6381587560c893757647651c1180c3
Owlynn <contact@jessalynn.fr>
2022-10-05 16:08:33 +0200

    correction d'une faute de frappe entrainant un bug

--- src/components/ListContainer/ListContainer.js
+++ src/components/ListContainer/ListContainer.js
@@ -3,16 +3,14 @@ import './ListContainer.css'
 import ListItem from "../ListItem/ListItem";
 import { v4 as uuidv4 } from 'uuid';
 
-//TODO
-// filtrer les taches
-// compter les tâches
-
 class ListContainer extends React.Component{
   constructor(props){
     super(props);
 
     this.state = {
         itemArray : [],
+        filteredArray: [],
+        mode : 'all',
         userInput : "",
         clearButtonIsDisplayed : false
       }
@@ -23,128 +21,180 @@ class ListContainer extends React.Component{
       this.checkTask = this.checkTask.bind(this);
       this.sortArray = this.sortArray.bind(this);
       this.clearCheckedTasks = this.clearCheckedTasks.bind(this);
-      this.displayClearButton = this.displayClearButton.bind(this);
+      this.displayFilterButtons = this.displayFilterButtons.bind(this);
       this.handleUserInputChange = this.handleUserInputChange.bind(this);
       this.countItems = this.countItems.bind(this);
-    }
-    
-    sortArray(itemArray){
-      return itemArray.sort((a, b) => a.isChecked < b.isChecked ? -1 : 1);
-    }
-    
-    pushInArray(event){
-      if (!this.state.userInput){
-        event.preventDefault()
-        return alert('the input field is empty')
-      }
-
-      let workingArray = this.state.itemArray;
-      workingArray.push({
-        id : uuidv4(),
-        isChecked : false,
-        label : this.state.userInput
-      })
-      
-      this.setState({ itemArray: workingArray })
+      this.toggleCheckedMode = this.toggleCheckedMode.bind(this);
+      this.toggleUncheckedMode = this.toggleUncheckedMode.bind(this);
+      this.filterCheckedTasks = this.filterCheckedTasks.bind(this);
+  }
+  
+  //regroupe les éléments en mettant les tâches cochées à la fin
+  sortArray(itemArray){
+    return itemArray.sort((a, b) => a.isChecked < b.isChecked ? -1 : 1);
+  }
+  
+  //ajoute un élément défini par l'user dans la liste.
+  pushInArray(event){
+    if (!this.state.userInput){
       event.preventDefault()
-      this.setState({userInput:''})
+      return alert('the input field is empty')
+    }
 
-    }
+    let workingArray = this.state.itemArray;
+    workingArray.push({
+      id : uuidv4(),
+      isChecked : false,
+      label : this.state.userInput
+    })
     
-    clearCheckedTasks(){
-      let workingArray = this.state.itemArray
-      let filteredArray = workingArray.filter(item => !item.isChecked )
-      this.setState({itemArray:filteredArray})
-      
-      if(filteredArray.every(element => !element.isChecked)){
-        this.setState(
-          {clearButtonIsDisplayed: false}
-        )
-      }
-    }
-    
-    checkTask(currentId){
-      let workingArray = this.state.itemArray
-      workingArray.forEach( element => {
-          if(element.id === currentId) {
-            element.isChecked = !element.isChecked;
-          }
-       }) 
-       
-      if (this.state.itemArray.some(element => element.isChecked)) {
-       this.setState({
-         clearButtonIsDisplayed : true})
-      }else{
-         this.setState({
-           clearButtonIsDisplayed : false})
-      };
+    this.setState({ itemArray: workingArray })
+    event.preventDefault()
+    this.setState({userInput:''})
 
-      this.setState({
-        itemArray : this.sortArray(workingArray)
-      })
-    }
+  }
+  
+  //supprime toutes les tâches déjà cochées.
+  clearCheckedTasks(){
+    console.log("cleared")
+    let workingArray = this.state.itemArray
+    let filteredArray = workingArray.filter(item => !item.isChecked )
+    this.setState({itemArray:filteredArray})
     
-    removeTask(buttonid){
-      let itemArray = this.state.itemArray
-      let workingArray = itemArray.filter(element => {
-        return element.id !== buttonid;
-      });
-      
-      this.setState({itemArray : workingArray})
-      
-      if(this.state.itemArray.length === 1){
-        console.log('égal 1')
-        this.setState({
-          clearButtonIsDisplayed:false
-        })
-      }
-    }
-    
-    renderItem(e){
-      return (
-        <ListItem key= {e.id} id ={e.id} label ={e.label} isChecked= {e.isChecked} checkTask = {this.checkTask} removeTask ={this.removeTask} />
-      )
-    }
-    
-    handleUserInputChange(e) {
-      this.setState({ userInput: e.target.value});
-    }
-    
-    displayClearButton (){
-      if (this.state.clearButtonIsDisplayed){
-        return <button className = "clear" onClick = {this.clearCheckedTasks}>Clear all checked task</button>
-      }
-    }
-    
-    countItems(){
-      console.log("length " + this.state.itemArray.length)
-        return (
-        <div>
-          <p className="taskCounter">Nombre de tâches à effectuer : {this.state.itemArray.length}</p>
-       </div>
-       )
-        }
-
-    render(){
-      return (
-        <div className='list-container'>
-          
-                  <form className = "item-input">
-                      <input type="text" id="text-input" name="text-input" placeholder='Add a new task' value={this.state.userInput} onChange={this.handleUserInputChange}/>
-                      <input type='submit' className="add-task" onClick={this.pushInArray}/>
-                  </form>
-
-          {this.displayClearButton()}
-          {this.state.itemArray.map(
-              (e) => {
-                return this.renderItem(e)
-              })
-          }
-          
-          {this.countItems()}
-        </div>
+    if(filteredArray.every(element => !element.isChecked)){
+      this.setState(
+        {clearButtonIsDisplayed: false}
       )
     }
   }
+  
+  // coche une task et la décoche si elle est déjà cochée.
+  checkTask(currentId){
+    let workingArray = this.state.itemArray;
+    workingArray.forEach( element => {
+        if(element.id === currentId) {
+          element.isChecked = !element.isChecked;
+        }
+      }) 
+      
+    if (this.state.itemArray.some(element => element.isChecked)) {
+      this.setState({
+        clearButtonIsDisplayed : true})
+    }else{
+        this.setState({
+          clearButtonIsDisplayed : false})
+    };
+
+    this.setState({
+      itemArray : this.sortArray(workingArray)
+    })
+  }
+  
+  // supprime une tâche
+  removeTask(elementId){
+    let itemArray = this.state.itemArray
+    let workingArray = itemArray.filter(element => {
+      return element.id !== elementId;
+    });
+    
+    this.setState({itemArray : workingArray})
+    
+    if(this.state.itemArray.length === 1){
+      this.setState({
+        clearButtonIsDisplayed:false
+      })
+    }
+  }
+  
+  // return un compsant ListItem (élement de la liste)
+  renderItem(e){
+    return (
+      <ListItem key= {e.id} id ={e.id} label ={e.label} isChecked= {e.isChecked} checkTask = {this.checkTask} removeTask ={this.removeTask} />
+    )
+  }
+  
+  // gère le changement de valeur de l'input
+  handleUserInputChange(e) {
+    this.setState({ userInput: e.target.value});
+  }
+  
+  // si nécessaire, affiche les boutons de filtre 
+  displayFilterButtons (){
+    if (this.state.clearButtonIsDisplayed){
+      return (
+        <div>
+          <button className = "clear" onClick = {this.clearCheckedTasks}> Clear all checked task</button>
+          <button className = "clear" onClick = {this.toggleCheckedMode}> Completed Tasks</button>
+          <button className = "clear" onClick = {this.toggleUncheckedMode}> Upcoming Tasks</button>
+        </div>
+      );
+    }
+  }
+  
+  toggleCheckedMode(){
+    this.setState({mode : 'checked'})
+  }
+
+  toggleUncheckedMode(){
+    this.setState({mode : 'unchecked'})
+  }
+
+  countItems(){
+      return (
+      <div>
+        <p id="taskCounter">Nombre de tâches à effectuer : {this.state.itemArray.length}</p>
+      </div>
+      )
+  }
+
+  filterCheckedTasks(){
+    let allModeArray = this.state.itemArray;
+
+    if (this.state.mode ==='checked'){
+      let checkedArray = allModeArray.map(item =>{
+        if(item.isChecked){
+            return item;
+          }else{
+            return null;
+          }
+      });
+
+      this.setState({filteredArray : checkedArray})
+
+    }else if (this.state.mode ==='unchecked'){
+      let uncheckedArray = allModeArray.map(item =>{
+        if(item.isChecked){
+            return item;
+          }else{
+            return null;
+          }
+      });
+        this.setState({filteredArray : uncheckedArray});
+    }else if(this.state.mode === 'all'){
+      this.setState({filteredArray : []})
+    }
+  }
+
+  render(){
+    return (
+      <div className='list-container'>
+        
+        <form className = "item-input">
+            <input type="text" id="text-input" name="text-input" placeholder='Add a new task' value={this.state.userInput} onChange={this.handleUserInputChange}/>
+            <input type='submit' className="add-task" onClick={this.pushInArray}/>
+        </form>
+        {this.displayFilterButtons()}
+        {
+          this.state.itemArray
+          .filter(e => e !== null)
+          .map(e => {
+              return this.renderItem(e)
+            })
+        }
+        {this.countItems()}
+      </div>
+      )
+  }
+}
   
 export default ListContainer;
